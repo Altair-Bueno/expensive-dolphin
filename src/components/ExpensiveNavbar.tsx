3261ff941d2f01c329c61686b331bacf4c7d368b
Altair-Bueno <67512202+Altair-Bueno@users.noreply.github.com>
2022-03-12 18:25:17 +0100

    new: Navbar

---
+++ src/components/ExpensiveNavbar.tsx
@@ -0,0 +1,49 @@
+import {Container, Form, FormControl, Nav, Navbar} from "react-bootstrap";
+
+import {LinkContainer} from 'react-router-bootstrap'
+import {Pages} from "../pages";
+
+export {
+    ExpensiveNavbar
+}
+
+function ExpensiveNavbar() {
+    return <Navbar bg="dark" variant = "dark" expand="lg" sticky="top">
+        <Container>
+            <Navbar.Brand>
+                <i className={"bi-currency-dollar"}/>
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="me-auto">
+                    <LinkContainer to={Pages.Home}>
+                        <Nav.Link>Today's Deals</Nav.Link>
+                    </LinkContainer>
+                    <LinkContainer to={Pages.Stores}>
+                        <Nav.Link>Stores</Nav.Link>
+                    </LinkContainer>
+                    <LinkContainer to={Pages.MyList}>
+                        <Nav.Link>My List</Nav.Link>
+                    </LinkContainer>
+                    <LinkContainer to={Pages.Help}>
+                        <Nav.Link>Help</Nav.Link>
+                    </LinkContainer>
+                </Nav>
+                <Form className="d-flex">
+                    <FormControl
+                        type="search"
+                        placeholder="Search"
+                        className="me-2"
+                        aria-label="Search"
+                    />
+                </Form>
+                <Navbar.Text>
+                    {/* FIXME */}
+                    <LinkContainer to={Pages.Profile}>
+                        <i className={"bi-person-circle text-light h5"}/>
+                    </LinkContainer>
+                </Navbar.Text>
+            </Navbar.Collapse>
+        </Container>
+    </Navbar>
+}
