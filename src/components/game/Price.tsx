b870fc0e82665780dfaff71a1b7e726862a77c93
Altair-Bueno <67512202+Altair-Bueno@users.noreply.github.com>
2022-03-17 22:08:37 +0100

    new: Price component

--- src/components/game/Price.tsx
+++ src/components/game/Price.tsx
@@ -1,31 +1,38 @@
+import './Price.css'
+
 export {
     Price
 }
 
 interface PriceProps {
     price: number,
-    retailPrice: number
+    retailPrice: number,
     savings: number,
     isOnSale: any
 }
 
 function Price(props: PriceProps) {
-    if (props.isOnSale) {
-        return <div className={"bg-dark"}>
-            <img src={"/"} alt={"Foo"}/>
-            <h3>${props.price}</h3>
-            <div className={"col"}>
-                <del className={"col-1"}>
-                    ${props.retailPrice}
-                </del>
-                <span className={"rounded rounded-2 bg-danger col-1"}>
-                    -{props.savings.toPrecision(2)}
+    const sale = props.isOnSale ?(
+        <div className={"col p-0 m-0 ps-2"}>
+            <div className={"row p-0 m-0"}>
+                <del className={"small p-0 m-0"}>${props.retailPrice}</del>
+            </div>
+            <div className={"row p-0 m-0 w-75 "}>
+                <span className={"rounded rounded-2 bg-danger small p-0 m-0 price-tag"} >
+                    -{props.savings.toPrecision(2)}%
                 </span>
             </div>
+        </div>) : <div className={"col"}/>
+
+
+    return (
+        <div className={"container price-root"}>
+            <div className={"row p-0 m-0"}>
+                <div className={"fs-1 col-9 p-0 m-0 text-end"}>
+                    ${props.price}
+                </div>
+                {sale}
+            </div>
         </div>
-    } else {
-        return <div>
-            <h3>${props.price}</h3>
-        </div>
-    }
+    )
 }
--- src/pages/Help.tsx
+++ src/pages/Help.tsx
@@ -1,7 +1,15 @@
+import {Price} from "../components/game/Price";
+
 export {
     Help
 }
 
 function Help() {
-    return <h1>Help placeholder</h1>
+    return <div className={"h-25"}>
+        <Price price={10} retailPrice={20} savings={50} isOnSale={true}/>
+        <Price price={100} retailPrice={400} savings={75} isOnSale={true}/>
+        <Price price={999} retailPrice={1} savings={75} isOnSale={true}/>
+        <Price price={1} retailPrice={20} savings={98} isOnSale={true}/>
+        <Price price={10} retailPrice={10} savings={0} isOnSale={false}/>
+    </div>
 }
