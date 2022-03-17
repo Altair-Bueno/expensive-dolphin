014eb1e4fb8265d735cfc21b14d10e56a4385217
Altair-Bueno <67512202+Altair-Bueno@users.noreply.github.com>
2022-03-17 20:23:32 +0100

    new: Navbar now includes icons

--- src/components/ExpensiveNavbar.tsx
+++ src/components/ExpensiveNavbar.tsx
@@ -8,48 +8,64 @@ export {
     ExpensiveNavbar
 }
 
-function ExpensiveNavbar() {
-    return <Navbar bg="dark" variant="dark" expand="lg" sticky="top">
+interface ExpensiveNavbarProps {
+    username:string|null
+}
+
+function ExpensiveNavbar(props:ExpensiveNavbarProps) {
+    const usernamePlaceholder = "My account"
+
+    const todayIcon = <i className={"bi-currency-dollar"}/>
+    const storesIcon = <i className={"bi-shop"}/>
+    const listIcon = <i className={"bi-bag"}/>
+    const personIcon = <i className={"bi-person-circle"}/>
+    const helpIcon = <i className={"bi-question-circle"}/>
+
+    return <Navbar
+                    bg="dark"
+                    variant="dark"
+                    expand="lg"
+                    sticky="top"
+                    collapseOnSelect={true}>
         <Container>
             <Navbar.Brand>
                 <img
                     src={logo}
                     height="25"
+                    alt={"logo"}
                 />
             </Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav"/>
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="me-auto">
                     <LinkContainer to={Pages.Home}>
-                        <Nav.Link>Today's Deals</Nav.Link>
+                        <Nav.Link>{todayIcon} Today's Deals</Nav.Link>
                     </LinkContainer>
                     <LinkContainer to={Pages.Stores}>
-                        <Nav.Link>Stores</Nav.Link>
+                        <Nav.Link>{storesIcon} Stores</Nav.Link>
                     </LinkContainer>
                     <LinkContainer to={Pages.MyList}>
-                        <Nav.Link>My List</Nav.Link>
+                        <Nav.Link>{listIcon} My List</Nav.Link>
                     </LinkContainer>
                     <LinkContainer to={Pages.Help}>
-                        <Nav.Link>Help</Nav.Link>
+                        <Nav.Link>{helpIcon} Help</Nav.Link>
+                    </LinkContainer>
+                    <LinkContainer to={Pages.Profile}>
+                        <Nav.Link>
+                            {personIcon} {
+                                (typeof props.username === "string") ?
+                                    props.username:
+                                    usernamePlaceholder
+                            }
+                        </Nav.Link>
                     </LinkContainer>
                 </Nav>
                 <Form className="d-flex">
                     <FormControl
                         type="search"
                         placeholder="Search"
-                        className="me-2"
-                        aria-label="Search"
-                    />
+                        aria-label="Search"/>
                 </Form>
-                <Navbar.Text>
-                    {/*
-                        FIXME icon placement and responsive behaviour
-                        FIXME clicking does not select the icon (see figma mockup)
-                     */}
-                    <LinkContainer to={Pages.Profile}>
-                        <i className={"bi-person-circle text-light h4"}/>
-                    </LinkContainer>
-                </Navbar.Text>
             </Navbar.Collapse>
         </Container>
     </Navbar>
