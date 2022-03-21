08da9504c97e37ec9eeedb88d91a2f54d2e83c18
José Luis Bueno <74137433+jxtaaa@users.noreply.github.com>
2022-03-21 16:36:06 +0100

    ExpensiveLoading now is responsive and bigger.

--- src/components/ExpensiveLoading.tsx
+++ src/components/ExpensiveLoading.tsx
@@ -1,11 +1,13 @@
+import './ExpendiseLoading.css'
+
 export {
     ExpensiveLoading
 }
 
 function ExpensiveLoading() {
     return <div className={"d-flex align-items-center text-light"}>
-        <div className={"spinner-border"} role={"status"} aria-hidden={"true"}/>
+        <div className={"spinner-border loading-gif"} role={"status"} aria-hidden={"true"}/>
         <div className={"mx-1"}/>
-        <strong>Loading...</strong>
+        <strong className={"loading-text"}>Loading...</strong>
     </div>
 }
