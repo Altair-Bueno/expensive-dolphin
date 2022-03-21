f71ff2d932447ebb594d1a604a314d1c03401ee3
José Luis Bueno <74137433+jxtaaa@users.noreply.github.com>
2022-03-21 17:32:11 +0100

    Updated ExpensiveNotFound tsx and css.

--- src/components/ExpensiveNotFound.tsx
+++ src/components/ExpensiveNotFound.tsx
@@ -1,10 +1,24 @@
+import Button from "react-bootstrap/Button";
+import './ExpensiveNotFound.css'
+
 export{
     ExpensiveNotFound
 }
 
-function ExpensiveNotFound(){
+interface NotFoundProps{
+    link : String
+
+}
+
+function ExpensiveNotFound(props : NotFoundProps){
     return(
-        <div>
+        <div className={"text-light"}>
+            <h1>Page Not Found</h1>
+            <h2>We couldn't find the page your were looking for</h2>
+            <small>{props.link}<br/></small>
+            <div className={"text-right"}>
+                <Button variant="primary" className={"colorbutton"} >Primary</Button>{' '}
             </div>
+        </div>
     );
 }
