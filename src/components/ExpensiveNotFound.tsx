541d2f68d96754d3fecab4797b06ed4f32667799
Altair-Bueno <67512202+Altair-Bueno@users.noreply.github.com>
2022-03-23 10:22:18 +0100

    new: NotFoundPage

--- src/components/ExpensiveNotFound.tsx
+++ src/components/ExpensiveNotFound.tsx
@@ -1,23 +1,34 @@
 import Button from "react-bootstrap/Button";
 import './ExpensiveNotFound.css'
+import {LinkContainer} from "react-router-bootstrap";
+import {Pages} from "../pages";
 
 export{
     ExpensiveNotFound
 }
 
 interface NotFoundProps{
-    link : String
-
+    link : string
 }
 
 function ExpensiveNotFound(props : NotFoundProps){
     return(
-        <div className={"text-light"}>
-            <h1>Page Not Found</h1>
-            <h2>We couldn't find the page your were looking for</h2>
-            <small>{props.link}<br/></small>
-            <div className={"text-right"}>
-                <Button variant="primary" className={"colorbutton"} >Primary</Button>{' '}
+        <div className={"text-light bg-dark rounded rounded-5 shadow p-5"}>
+            <div className="container">
+                <div className={"row mb-3"}>
+                    <h1>Page Not Found</h1>
+                </div>
+                <div className={"row mb-3"}>
+                    <h2>We couldn't find the page your were looking for</h2>
+                </div>
+                <div className={"row mb-3"}>
+                    <small >{props.link}</small>
+                </div>
+                <div className={"row-1 mb-3"}>
+                    <LinkContainer to={Pages.Home}>
+                        <Button variant="primary">Return Home</Button>
+                    </LinkContainer>
+                </div>
             </div>
         </div>
     );
