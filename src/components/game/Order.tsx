da55c8d8d3c5bea3f70274a8c001b2298caee19c
Altair-Bueno <67512202+Altair-Bueno@users.noreply.github.com>
2022-03-30 21:54:40 +0200

    fix: Order arrows no longer resize when switched mode

--- src/components/game/Order.tsx
+++ src/components/game/Order.tsx
@@ -16,14 +16,16 @@ function Order (props:OrderProps) {
     const all = props.order === OrderEnum.NONE;
     const up = all || props.order === OrderEnum.ASCENDING
     const down = all || props.order === OrderEnum.DESCENDING;
+    const opacityZero = 'opacity-0'
+    const opacityFull = 'opacity-100'
 
     return <div className={"text-light d-flex small"}>
         <div className={"col m-0 p-0"}>
             <div className={"row m-0 p-0"}>
-                {up ? <i className = {"bi-caret-up-fill m-0 p-0"}/>:<div className={"p-2"}/>}
+                <i className = {`bi-caret-up-fill m-0 p-0 ${up ? opacityFull:opacityZero}`}/>
             </div>
             <div className={"row m-0 p-0"}>
-                {down ? <i className = {"bi-caret-down-fill m-0 p-0"}/>:<div className={"p-2"}/>}
+                <i className = {`bi-caret-down-fill m-0 p-0 ${down ? opacityFull:opacityZero}`}/>
             </div>
         </div>
     </div>
