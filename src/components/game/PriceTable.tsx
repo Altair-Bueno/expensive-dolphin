4af25cd283ef972126f68788f7fcbdea86ca615d
cyber666 <franhm@uma.es>
2022-04-03 22:06:50 +0200

    Query try

--- src/components/game/PriceTable.tsx
+++ src/components/game/PriceTable.tsx
@@ -13,207 +13,140 @@ const baseapiURL = "https://www.cheapshark.com/api/1.0";
 const redirectDeal = "https://www.cheapshark.com/redirect?dealID=";
 const headers = ["Shop", "Current Price", "Deal", "Retail Price", "Link"];
 
-interface PriceTableProps {
-        tablemodel: ListOfDeals[];
-        storeModel: Store[];
+export interface PriceTableProps {
+  tablemodel: ListOfDeals[];
+  storeModel: Store[];
 }
 
 function toFixed(value: number, precision: number) {
-        var precision = precision || 0,
-                power = Math.pow(10, precision),
-                absValue = Math.abs(Math.round(value * power)),
-                result =
-                        (value < 0 ? "-" : "") +
-                        String(Math.floor(absValue / power));
+  var precision = precision || 0,
+    power = Math.pow(10, precision),
+    absValue = Math.abs(Math.round(value * power)),
+    result = (value < 0 ? "-" : "") + String(Math.floor(absValue / power));
 
-        if (precision > 0) {
-                var fraction = String(absValue % power),
-                        padding = new Array(
-                                Math.max(precision - fraction.length, 0) + 1
-                        ).join("0");
-                result += "." + padding + fraction;
-        }
-        return result;
+  if (precision > 0) {
+    var fraction = String(absValue % power),
+      padding = new Array(Math.max(precision - fraction.length, 0) + 1).join(
+        "0"
+      );
+    result += "." + padding + fraction;
+  }
+  return result;
 }
 
 function getStore(id: string, stores: Store[]) {
-        return stores.find((element) => element.storeID === id)!;
+  return stores.find((element) => element.storeID === id)!;
 }
 
 function openInNewTab(url: string) {
-        window.open(url, "_blank")?.focus();
+  window.open(url, "_blank")?.focus();
 }
 
 function PriceTable(props: PriceTableProps) {
-        
-        const [ordered, setOrdered] = useState({
-                order: OrderEnum.NONE,
-                rownum: 0,
-        });
+  const [ordered, setOrdered] = useState({
+    order: OrderEnum.NONE,
+    rownum: 0,
+  });
 
-        const orig = [...props.tablemodel];
-        var copy = [...props.tablemodel];
-        const origarrwos = Order({ order: OrderEnum.NONE });
+  const orig = [...props.tablemodel];
+  var copy = [...props.tablemodel];
+  const origarrwos = Order({ order: OrderEnum.NONE });
 
-        var sorted = sortData(copy, ordered);
+  var sorted = sortData(copy, ordered);
 
-        var final = ordered.order === OrderEnum.NONE ? [...orig] : [...sorted];
+  var final = ordered.order === OrderEnum.NONE ? [...orig] : [...sorted];
 
-        return (
-                <Table
-                        striped
-                        bordered
-                        hover
-                        variant="dark"
-                        className="table align-middle text-center"
+  return (
+    <Table
+      striped
+      bordered
+      hover
+      variant="dark"
+      className="table align-middle text-center"
+    >
+      <thead>
+        <tr className="text-center">
+          {headers.map((element, index) => {
+            {
+              var res;
+              index === 0 || index === 4
+                ? (res = (
+                    <th key={index} className="align-middle text-center m-auto">
+                      {element}
+                    </th>
+                  ))
+                : (res = (
+                    <th key={index} className="m-auto text-center">
+                      <button
+                        type="button"
+                        className="btn text-light shadow-none"
+                        onClick={() => {
+                          setOrdered({
+                            order:
+                              ordered.rownum === index
+                                ? (ordered.order + 1) % 3
+                                : 1,
+                            rownum: index,
+                          });
+                        }}
+                      >
+                        <div className="d-flex justify-content-center">
+                          <div className="my-auto pe-3">{element}</div>
+                          {index === ordered.rownum
+                            ? Order(ordered)
+                            : origarrwos}
+                        </div>
+                      </button>
+                    </th>
+                  ));
+
+              return res;
+            }
+          })}
+        </tr>
+      </thead>
+      <tbody>
+        {final.map((element) => {
+          return (
+            <tr key={final.indexOf(element)}>
+              <td>
+                <img
+                  className="logo"
+                  src={
+                    baseimgsURL +
+                    getStore(element.storeID, props.storeModel).images.banner
+                  }
+                ></img>
+                <img
+                  className="logo2"
+                  src={
+                    baseimgsURL +
+                    getStore(element.storeID, props.storeModel).images.logo
+                  }
+                ></img>
+              </td>
+              <td>${element.salePrice}</td>
+              <td>{toFixed(parseFloat(element.savings), 2)}%</td>
+              <td>${element.normalPrice}</td>
+              <td>
+                <Button
+                  variant="primary"
+                  onClick={() => {
+                    openInNewTab(`${redirectDeal}${element.dealID}`);
+                  }}
+                  bsPrefix="btn"
+                  className={"btn-primary"}
                 >
-                        <thead>
-                                <tr className="text-center">
-                                        {headers.map((element, index) => {
-                                                {
-                                                        var res;
-                                                        index === 0 ||
-                                                        index === 4
-                                                                ? (res = (
-                                                                          <th
-                                                                                  key={
-                                                                                          index
-                                                                                  }
-                                                                                  className="align-middle text-center m-auto"
-                                                                          >
-                                                                                  {
-                                                                                          element
-                                                                                  }
-                                                                          </th>
-                                                                  ))
-                                                                : (res = (
-                                                                          <th
-                                                                                  key={
-                                                                                          index
-                                                                                  }
-                                                                                  className="m-auto text-center"
-                                                                          >
-                                                                                  <button
-                                                                                          type="button"
-                                                                                          className="btn text-light shadow-none"
-                                                                                          onClick={() => {
-                                                                                                  setOrdered(
-                                                                                                          {
-                                                                                                                  order:
-                                                                                                                          ordered.rownum ===
-                                                                                                                          index
-                                                                                                                                  ? (ordered.order +
-                                                                                                                                            1) %
-                                                                                                                                    3
-                                                                                                                                  : 1,
-                                                                                                                  rownum: index,
-                                                                                                          }
-                                                                                                  );
-                                                                                          }}
-                                                                                  >
-                                                                                          <div className="d-flex justify-content-center">
-                                                                                                  <div className="my-auto pe-3">
-                                                                                                          {
-                                                                                                                  element
-                                                                                                          }
-                                                                                                  </div>
-                                                                                                  {index ===
-                                                                                                  ordered.rownum
-                                                                                                          ? Order(
-                                                                                                                    ordered
-                                                                                                            )
-                                                                                                          : origarrwos}
-                                                                                          </div>
-                                                                                  </button>
-                                                                          </th>
-                                                                  ));
-
-                                                        return res;
-                                                }
-                                        })}
-                                </tr>
-                        </thead>
-                        <tbody>
-                                {final.map((element) => {
-                                        return (
-                                                <tr key={final.indexOf(element)}>
-                                                        <td>
-                                                                <img
-                                                                        className="logo"
-                                                                        src={
-                                                                                baseimgsURL +
-                                                                                getStore(
-                                                                                        element.storeID,
-                                                                                        props.storeModel
-                                                                                )
-                                                                                        .images
-                                                                                        .banner
-                                                                        }
-                                                                ></img>
-                                                                <img
-                                                                        className="logo2"
-                                                                        src={
-                                                                                baseimgsURL +
-                                                                                getStore(
-                                                                                        element.storeID,
-                                                                                        props.storeModel
-                                                                                )
-                                                                                        .images
-                                                                                        .logo
-                                                                        }
-                                                                ></img>
-                                                        </td>
-                                                        <td>
-                                                                $
-                                                                {
-                                                                        element.salePrice
-                                                                }
-                                                        </td>
-                                                        <td>
-                                                                {toFixed(
-                                                                        parseFloat(
-                                                                                element.savings
-                                                                        ),
-                                                                        2
-                                                                )}
-                                                                %
-                                                        </td>
-                                                        <td>
-                                                                $
-                                                                {
-                                                                        element.normalPrice
-                                                                }
-                                                        </td>
-                                                        <td>
-                                                                <Button
-                                                                        variant="primary"
-                                                                        onClick={() => {
-                                                                                openInNewTab(
-                                                                                        `${redirectDeal}${element.dealID}`
-                                                                                );
-                                                                        }}
-                                                                        bsPrefix="btn"
-                                                                        className={
-                                                                                "btn-primary"
-                                                                        }
-                                                                >
-                                                                        <div
-                                                                                className={
-                                                                                        "d-none d-md-block"
-                                                                                }
-                                                                        >
-                                                                                Go
-                                                                                to
-                                                                                website{" "}
-                                                                                <i className="bi bi-arrow-up-right-square" />
-                                                                        </div>
-                                                                        <i className="d-md-none bi bi-arrow-up-right-square" />
-                                                                </Button>
-                                                        </td>
-                                                </tr>
-                                        );
-                                })}
-                        </tbody>
-                </Table>
-        );
+                  <div className={"d-none d-md-block"}>
+                    Go to website <i className="bi bi-arrow-up-right-square" />
+                  </div>
+                  <i className="d-md-none bi bi-arrow-up-right-square" />
+                </Button>
+              </td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </Table>
+  );
 }
--- src/pages/MyList.tsx
+++ src/pages/MyList.tsx
@@ -1,18 +1,92 @@
 import { PriceTable } from "../components/game/PriceTable";
-import { fetchDeals, getStores } from "../components/Query"
+import { ExpensiveLoading } from "../components/ExpensiveLoading";
+import { AlertType, ExpensiveAlert } from "../components/ExpensiveAlert";
+import { dealsBaseAPIURL, storesBaseAPIURL } from "../components/Query";
 import { deals, stores } from "../cheapshark/exampledata";
-
-
+import { Store } from "../cheapshark/stores"
+import { LoadError } from "../components/LoadError";
+import { ListOfDeals } from "../cheapshark/deals/listOfDeals";
+import { useEffect, useState } from "react";
 
 export { MyList };
 
+// For the "unwrapping" variation
+
+async function api<T>(url: string): Promise<T> {
+  return fetch(url)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+      return response.json() as Promise<{ data: T }>;
+    })
+    .then((data) => {
+      return data.data;
+    });
+}
+
+
+
 function MyList() {
+    const [data, setData] = useState({
+        tiendas: {} as Store[],
+        ofertas: {} as ListOfDeals[],
+        error: false
+    });
+
+    useEffect(() => {
+        const fetchData =  async () => {
+            try{
+                const [respTodoOne, respTodoTwo] = await Promise.all([
+                    fetch(dealsBaseAPIURL),
+                    fetch(storesBaseAPIURL)
+                ]);
+                
+                const respT = await respTodoOne.json();
+                const respD = await respTodoTwo.json();
+
+                console.log(respT.json())
+                console.log(respD.json())
+
+                setData({
+                    tiendas: respT as Store[],
+                    ofertas: respD as ListOfDeals[],
+                    error: false
+                })
+        
+                
+            } catch(err){
+                setData({
+                    tiendas: {} as Store[],
+                    ofertas: {} as ListOfDeals[],
+                    error: true
+                })
+            }
+        }
+
+    })
+
+    if(data.error){
+        return (
+            <>
+                    {LoadError()}
+                    {ExpensiveAlert({
+                            alertType: AlertType.danger,
+                            title: "Loading error",
+                            content: "An unexpected error occured while loading the data. Reaload the website to try again.",
+                            buttonText: "Close",
+                    })}
+            </>
+    );
+    }
+
     return (
         <>
-            <h1>My List placeholder</h1>
-            <div className="col-md-10 offset-md-1">
-                <PriceTable storeModel={stores} tablemodel={deals} />
-            </div>
+          <h1>My List placeholder</h1>
+          <div className="col-md-10 offset-md-1">
+            <PriceTable storeModel={data.tiendas} tablemodel={data.ofertas} />
+          </div>
         </>
-    );
+      );
+  
 }
