8e3ef43857a587ad0b198bc073aef0029ab877a6
cyber666 <franhm@uma.es>
2022-04-03 20:56:17 +0200

    API URLS added

---
+++ src/cheapshark/apiurls.ts
@@ -0,0 +1,4 @@
+export const baseimgsURL = "https://www.cheapshark.com";
+export const baseapiURL = "https://www.cheapshark.com/api/1.0";
+export const dealsBaseAPIURL = "https://www.cheapshark.com/api/1.0/deals";
+export const redirectDeal = "https://www.cheapshark.com/redirect?dealID=";
