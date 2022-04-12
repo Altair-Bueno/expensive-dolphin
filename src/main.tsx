eb336d84d20e5e497ea88c456d4b2da0f9914df6
Altair-Bueno <67512202+Altair-Bueno@users.noreply.github.com>
2022-04-12 21:54:33 +0200

    fix: Top level await

--- src/main.tsx
+++ src/main.tsx
@@ -5,20 +5,19 @@ import ReactDOM from "react-dom";
 
 import {ExpensiveNavbar} from "./components/ExpensiveNavbar";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
-import {Help, Home, MyList, NotFound, Pages, Profile, Search,} from "./pages";
+import {Help, Home, NotFound, Pages, Profile, Search,} from "./pages";
 import {QueryClient, QueryClientProvider} from "react-query";
 import {storesURL} from "./cheapshark/stores";
 import {Store} from "./cheapshark/stores/stores";
 import {StoresContext} from "./ExpensiveContext";
 
 const queryClient = new QueryClient()
-const stores = await queryClient.fetchQuery([storesURL, undefined], async () => await fetch(storesURL).then(async x => await x.json() as Store[]))
 const expensiveRouter = (
     <BrowserRouter>
         <ExpensiveNavbar/>
         <Routes>
             <Route path={Pages.Home} element={<Home/>}/>
-            <Route path={Pages.MyList} element={<MyList/>}/>
+            {/*<Route path={Pages.MyList} element={<MyList/>}/>*/}
             <Route path={Pages.Help} element={<Help/>}/>
             <Route path={Pages.Search} element={<Search/>}/>
             <Route path={Pages.Profile} element={<Profile/>}/>
@@ -26,16 +25,20 @@ const expensiveRouter = (
         </Routes>
     </BrowserRouter>
 )
+async function main() {
+    const stores = await queryClient.fetchQuery([storesURL, undefined], async () => await fetch(storesURL).then(async x => await x.json() as Store[]))
+    ReactDOM.render(
+        <React.StrictMode>
+            <QueryClientProvider client={queryClient}>
+                <StoresContext.Provider value={stores}>
+                    <div id={"page-content"}>
+                        {expensiveRouter}
+                    </div>
+                </StoresContext.Provider>
+            </QueryClientProvider>
+        </React.StrictMode>,
+        document.getElementById("root")
+    )
+}
 
-ReactDOM.render(
-    <React.StrictMode>
-        <QueryClientProvider client={queryClient}>
-            <StoresContext.Provider value={stores}>
-                <div id={"page-content"}>
-                    {expensiveRouter}
-                </div>
-            </StoresContext.Provider>
-        </QueryClientProvider>
-    </React.StrictMode>,
-    document.getElementById("root")
-);
+main()
