1018312b8e235afc9f6697bb3c58291b374c9c93
Altair-Bueno <67512202+Altair-Bueno@users.noreply.github.com>
2022-04-12 17:40:32 +0200

    new: Rating filter

---
+++ src/components/search/RatingFilter.tsx
@@ -0,0 +1,21 @@
+export interface RatingFilterProps {
+    rating:number
+    maxRating:number
+    setRating: (rating: number) => void
+}
+
+export function RatingFilter(props: RatingFilterProps) {
+    const {rating, setRating, maxRating} = props;
+
+    const commonClasses = 'text-warning h5'
+    const fillIcon = 'bi-star-fill'
+    const emptyIcon = 'bi-star'
+    const stars = [
+        ...Array.from({length: rating}).map(() => fillIcon),
+        ...Array.from({length: maxRating - rating }).map(() => emptyIcon)
+    ].map((value, index) => <i className={`${commonClasses} ${value}`}
+                               onClick={() => setRating(index)}/>)
+    return (
+        <div>{stars}</div>
+    );
+}
