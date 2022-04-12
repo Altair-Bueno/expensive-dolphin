193620bc8d1b9f50a1d3a5666231282325310d05
Altair-Bueno <67512202+Altair-Bueno@users.noreply.github.com>
2022-04-12 18:24:34 +0200

    new: Improved filters

--- src/components/search/Filter.tsx
+++ src/components/search/Filter.tsx
@@ -1,11 +1,4 @@
-import {
-    Form,
-    FormCheck,
-    FormControl,
-    FormGroup,
-    FormSelect
-} from "react-bootstrap";
-import {ListOfDealsParam} from "../../cheapshark/deals";
+import {ListOfDealsParam, SortByOptions} from "../../cheapshark/deals";
 import {PriceRangeSlider} from "./PriceRangeSlider";
 import {RatingFilter, RatingFilterProps} from "./RatingFilter";
 
@@ -21,41 +14,58 @@ export interface FilterProps {
 function Filter(props: FilterProps) {
     const {setFilter, filter} = props
 
-    const ratingProps:RatingFilterProps = {
+    const steamRating:RatingFilterProps = {
         maxRating: 5, rating: (filter.steamRating || 0) + 1,
         setRating: (x)=> setFilter({...filter,steamRating:x})
     }
-    return <Form onSubmit={x=>x.preventDefault()}>
-        <FormGroup>
-            <Form.Label>Title</Form.Label>
-            <FormControl type={"search"} onChange={x => setFilter({ ...filter, title: x.target.value })}/>
-        </FormGroup>
-        <FormGroup>
-            <Form.Label>Price range: From {filter.lowerPrice} to {filter.upperPrice}</Form.Label>
+    const metacriticRating:RatingFilterProps = {
+        maxRating:5, rating: (filter.metacritic || 0) + 1,
+        setRating: (x)=>setFilter({ ...filter, metacritic: x})
+    }
+
+    return <form onSubmit={x=>x.preventDefault()}>
+        <div>
+            <label>Title</label>
+            <input type={"search"} onChange={x => setFilter({ ...filter, title: x.target.value })}/>
+        </div>
+        <div>
+            <label>Price range: From {filter.lowerPrice} to {filter.upperPrice}</label>
             <PriceRangeSlider max={999} min={0} step={10} onchange={x => setFilter({ ...filter, ...x })}/>
-        </FormGroup>
+        </div>
         <br/>
-        <FormGroup>
-            <RatingFilter {...ratingProps}/>
-        </FormGroup>
-        <FormGroup>
-            <FormCheck type={"checkbox"} label={"On sale"} onChange={x=> setFilter({ ...filter, onSale: x.currentTarget.checked})}/>
-        </FormGroup>
-        <FormGroup>
-            <FormCheck type={"checkbox"} label={"Steam Works"} onChange={x=> setFilter({ ...filter, steamworks: x.currentTarget.checked})}/>
-        </FormGroup>
-        <FormGroup>
-            <FormCheck type={"checkbox"} label={"AAA"} onChange={x=> setFilter({ ...filter, AAA: x.currentTarget.checked})}/>
-        </FormGroup>
-        <FormGroup>
-            <Form.Label>Sort By</Form.Label>
-            <FormSelect onChange={x=> setFilter({...filter,sortBy:x.currentTarget.value})}>
-                <option>Hello</option>
-                <option>By</option>
-            </FormSelect>
-        </FormGroup>
-        <FormGroup>
-            <FormCheck type={"checkbox"} label={"Descending"} onChange={x=> setFilter({ ...filter, desc: x.currentTarget.checked})}/>
-        </FormGroup>
-    </Form>
+        <div>
+            <label>Metacritic rating</label>
+            <RatingFilter {...metacriticRating}/>
+        </div>
+        <br/>
+        <div>
+            <label>Steam rating</label>
+            <RatingFilter {...steamRating}/>
+        </div>
+        <div>
+            <label>On Sale</label>
+            <input type={"checkbox"}  onChange={x=> setFilter({ ...filter, onSale: x.currentTarget.checked})}/>
+        </div>
+        <div>
+            <label>Steam works</label>
+            <input type={"checkbox"} onChange={x=> setFilter({ ...filter, steamworks: x.currentTarget.checked})}/>
+        </div>
+        <div>
+            <label>AAA</label>
+            <input type={"checkbox"} onChange={x=> setFilter({ ...filter, AAA: x.currentTarget.checked})}/>
+        </div>
+        <div>
+            <label>Sort By</label>
+            <select onChange={x=> setFilter({...filter,sortBy:x.currentTarget.value as SortByOptions})}>
+                {
+                    Object.entries(SortByOptions)
+                        .map(([_,value])=><option>{value}</option>)
+                }
+            </select>
+        </div>
+        <div>
+            <label>Descending</label>
+            <input type={"checkbox"} onChange={x=> setFilter({ ...filter, desc: x.currentTarget.checked})}/>
+        </div>
+    </form>
 }
