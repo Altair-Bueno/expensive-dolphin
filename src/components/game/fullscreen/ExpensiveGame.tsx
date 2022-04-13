d095a881dbaafbf51c8e8c9d9bef1c81ea5dccd5
Altair-Bueno <67512202+Altair-Bueno@users.noreply.github.com>
2022-04-13 21:45:57 +0200

    ref: ExpensiveGame.tsx now receives all data from props

--- src/components/game/fullscreen/ExpensiveGame.tsx
+++ src/components/game/fullscreen/ExpensiveGame.tsx
@@ -1,84 +1,69 @@
 import {PriceTable} from "./PriceTable";
-import {ExpensiveError} from "../../wrappers/ExpensiveError";
-import {AlertType, ExpensiveAlert} from "../../wrappers/ExpensiveAlert";
-import {ExpensiveLoading} from "../../wrappers/ExpensiveLoading";
 import {Price} from "../common/Price";
 import {Rating} from "../common/Rating";
 import Button from "react-bootstrap/Button";
-import {useCheapShark} from "../../../cheapshark";
-import {GameLookupParam, gamesURL} from "../../../cheapshark/games";
 import {Deal} from "../../../cheapshark/deals/listOfDeals";
 import {useContext} from "react";
 import {StoresContext} from "../../../types";
+import {GameLookup} from "../../../cheapshark/games/gameLookup";
 
 export {ExpensiveGame};
 
 interface ExpensiveGameProps {
-    oferta: Deal
+    deal: Deal,
+    gameLookup: GameLookup
 }
 
 function ExpensiveGame(props: ExpensiveGameProps) { // Game ID for lookup
-    const query: GameLookupParam = {id: Number.parseInt(props.oferta.gameID)}
-    const juego = useCheapShark(gamesURL, query);
+    const {deal, gameLookup} = props;
     const stores = useContext(StoresContext)
 
-    if (juego.isLoading) {
-        return <ExpensiveLoading/>;
-    } else if (juego.data) {
-        return (
-            <div className={"container-sm bg-secondary"}>
-                <div className="row-6 d-flex">
-                    <div className="col-2 m-0 p-0 d-flex justify-content-center">
-                        <img src={juego.data.info.thumb} className={"img-fluid"}/>
-                    </div>
+    const priceProps = {
+        price:Number.parseFloat(deal.salePrice),
+        retailPrice:Number.parseFloat(deal.normalPrice),
+        savings:Number.parseInt(deal.savings),
+        isOnSale:deal.isOnSale
+    }
+    const ratingProps = { steamRatingPercent:Number.parseInt(deal.steamRatingPercent) }
+    const priceTableProps = { storeModel: stores, tablemodel:gameLookup.deals }
 
-                    <div className="col-6 text-light m-2 p-2 justify-content-start">
-                        <div className="row">{juego.data.info.title}</div>
-                        <div className="row">
-                            <Rating
-                                steamRatingPercent={Number.parseInt(props.oferta.steamRatingPercent)}
-                            />
-                        </div>
-                        <div className="row">
-                            <div className="col-5 m-0 p-0 justify-content-start">
-                                <Price
-                                    price={Number.parseFloat(props.oferta.salePrice)}
-                                    retailPrice={Number.parseFloat(props.oferta.normalPrice)}
-                                    savings={Number.parseInt(props.oferta.savings)}
-                                    isOnSale={1}
-                                />
-                            </div>
-                        </div>
+    return <div className={"container-sm bg-secondary"}>
+        <div className="row-6 d-flex">
+            <div className="col-2 m-0 p-0 d-flex justify-content-center">
+                <img src={gameLookup.info.thumb} className={"img-fluid"} alt={gameLookup.info.title}/>
+            </div>
 
-                        <div className="row">
-                            <div className="col-3 m-0 p-0">
-                                <Button variant={"primary"}>On Steam</Button>
-                            </div>
-                            <div className="col-3 m-0 p-0">
-                                <Button variant={"primary"}>Add to list</Button>
-                            </div>
-                        </div>
+            <div className="col-6 text-light m-2 p-2 justify-content-start">
+                <div className="row">
+                    {gameLookup.info.title}
+                </div>
+                <div className="row">
+                    <Rating {...ratingProps}/>
+                </div>
+                <div className="row">
+                    <div className="col-5 m-0 p-0 justify-content-start">
+                        <Price {...priceProps}/>
                     </div>
                 </div>
-
-                <div className="row-6 d-flex">
-                    <div className="col-6">
-                        <PriceTable storeModel={stores} tablemodel={juego.data.deals}/>
+                <div className="row">
+                    <div className="col-3 m-0 p-0">
+                        <Button variant={"primary"}>
+                            On Steam
+                        </Button>
+                    </div>
+                    <div className="col-3 m-0 p-0">
+                        <Button variant={"primary"}>
+                            Add to list
+                        </Button>
                     </div>
                 </div>
             </div>
-        );
-    } else {
-        const alert = {
-            alertType: AlertType.danger,
-            title: "Loading error",
-            content: "An unexpected error occurred while loading the data. Reload the website to try again.",
-            buttonText: "Close",
-        }
-        return <>
-            <ExpensiveError/>
-            <ExpensiveAlert {...alert}/>
-        </>
-    }
+        </div>
 
+        <div className="row-6 d-flex">
+            <div className="col-6">
+                <PriceTable {...priceTableProps}/>
+            </div>
+        </div>
+    </div>
 }
--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,38 +1,5 @@
-import {ExpensiveGame} from "../components/game/fullscreen/ExpensiveGame";
-import {dealsURL, ListOfDealsParam} from "../cheapshark/deals";
-import {useCheapShark} from "../cheapshark";
-import {ExpensiveLoading} from "../components/wrappers/ExpensiveLoading";
-import {AlertType, ExpensiveAlert} from "../components/wrappers/ExpensiveAlert";
-import {ExpensiveError} from "../components/wrappers/ExpensiveError";
-
 export {Profile};
 
 function Profile() {
-    const query: ListOfDealsParam = {}
-    const ofertas = useCheapShark(dealsURL, query);
-
-    if (ofertas.isLoading) {
-        return <ExpensiveLoading/>;
-    } else if (ofertas.data) {
-        return (
-            <>
-                <ExpensiveGame oferta={ofertas.data[0]}/>
-            </>
-        );
-    } else {
-        const alert = {
-            alertType: AlertType.danger,
-            title: "Loading error",
-            content: "An unexpected error occurred while loading the data. Reload the website to try again.",
-            buttonText: "Close",
-        }
-        return (
-            <>
-                <ExpensiveError/>
-                <ExpensiveAlert {...alert}/>
-            </>
-        );
-    }
-
-
+    return <h1>WIP</h1>
 }
