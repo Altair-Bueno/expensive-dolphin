4686a2c648a7bf7cd54fae27d57525b35f1b7b58
Altair-Bueno <67512202+Altair-Bueno@users.noreply.github.com>
2022-04-14 15:21:52 +0200

    new (types): custom hooks for managing favourites and search history

--- src/types.ts
+++ src/types.ts
@@ -2,6 +2,7 @@ import {Store} from "./cheapshark/stores/stores";
 import React from "react";
 import {Location} from "react-router-dom";
 import useLocalStorageState from "use-local-storage-state";
+import {ListOfDealsParam} from "./cheapshark/deals";
 
 const defaultStores: Store[] = []
 export const StoresContext = React.createContext(defaultStores)
@@ -9,6 +10,19 @@ export const StoresContext = React.createContext(defaultStores)
 export interface User {
     name: string,
     email: string
+}
+export type SearchHistory = ListOfDealsParam []
+export type Favourites = Map<string,Date>
+
+export function useFavourites(): [Favourites, (searchHistory: Favourites) => void] {
+    const [favourites,setFavourites] = useLocalStorageState('favourites') as any as [Favourites | undefined, (searchHistory: Favourites) => void];
+    const defaultFavourites = new Map() as Favourites
+    return [favourites ? favourites : defaultFavourites, setFavourites]
+}
+
+export function useSearchHistory():[SearchHistory, (searchHistory: SearchHistory) => void] {
+    const [searchHistory,setSearchHistory] = useLocalStorageState('searchHistory') as any as [SearchHistory | undefined, (searchHistory: SearchHistory) => void];
+    return [searchHistory ? searchHistory : [], setSearchHistory]
 }
 
 export function useExpensiveUser() {
