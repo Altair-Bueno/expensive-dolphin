509ea0f6e32e6a5e99e08af71e0d91d05047af93
Altair-Bueno <67512202+Altair-Bueno@users.noreply.github.com>
2022-04-14 14:56:32 +0200

    new: React hook for User persistence

--- src/types.ts
+++ src/types.ts
@@ -1,9 +1,19 @@
 import {Store} from "./cheapshark/stores/stores";
 import React from "react";
 import {Location} from "react-router-dom";
+import useLocalStorageState from "use-local-storage-state";
 
 const defaultStores: Store[] = []
 export const StoresContext = React.createContext(defaultStores)
+
+export interface User {
+    name: string,
+    email: string
+}
+
+export function useExpensiveUser() {
+    return useLocalStorageState('user') as any as [User | undefined, (user: User) => void];
+}
 
 export interface LocationState {
     backgroundLocation?: Location
