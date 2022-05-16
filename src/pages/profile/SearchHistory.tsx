0a0cd8ea90232361481f659fc07443568faedeb1
José Luis Bueno <jlbp@uma.es>
2022-05-16 12:02:07 +0200

    Added "Clear history" button

--- src/pages/profile/SearchHistory.tsx
+++ src/pages/profile/SearchHistory.tsx
@@ -1,6 +1,10 @@
 import {useSearchHistory} from "../../types";
 
 export function SearchHistory() {
-    const [searchHistory,_] = useSearchHistory()
-    return <h1>{JSON.stringify(searchHistory)}</h1>
+    let [searchHistory,setSearchHistory] = useSearchHistory()
+
+    return <div>
+        <button onClick={x=>setSearchHistory([])} className={"btn btn-primary"}>Clear search history</button>
+        <h3 className={"text-light"}>{searchHistory.map(x => <>{x.title} <br/></>)}</h3>
+        </div>
 }
