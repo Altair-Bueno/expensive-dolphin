dac5a743831cfd8d88aba04157287b7e1b452206
José Luis Bueno <jlbp@uma.es>
2022-05-30 13:21:17 +0200

    Solved price container row.

--- src/components/game/common/Price.tsx
+++ src/components/game/common/Price.tsx
@@ -14,7 +14,7 @@ function Price(props: PriceProps) {
             <div className={"row p-0 m-0"}>
                 <del className={"small text-start p-0 m-0"}>${props.retailPrice}</del>
             </div>
-            <div className={"row p-0 m-0 w-76"}>
+            <div className={"row-2 p-0 m-0 w-76"}>
                 <span className={" rounded rounded-2 bg-danger small text-start p-1 mt-1 h-auto w-auto"} style={{color: "black"}}>
                     -{props.savings}%
                 </span>
